00fcbe6b0d93ad519c9a11b016221afe7ed7e5fa
JanderHungrige <32099752+JanderHungrige@users.noreply.github.com>
2020-10-06 11:32:39 +0200

    Delete version.py

--- version.py
+++
@@ -1,8 +0,0 @@
-__version__ = '0.7'
-
-def pretty_versions():
-    #import keras
-    import tensorflow as tf
-    #k_version = keras.__version__
-    t_version = tf.__version__
-    return "keras-vggface : {}, tensorflow : {} ".format(__version__,t_version)
